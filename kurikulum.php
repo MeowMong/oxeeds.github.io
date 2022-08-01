71c3f7c257c5f94527510e8e001ec4d71a177807
Rasyid_ <Rasyid.dot.id@gmail.com>
2022-08-01 12:17:09 -1000

    Update kurikulum.php
    Atur layout dan menambahkan konten

--- kurikulum.php
+++ kurikulum.php
@@ -4,26 +4,27 @@
 <!-- Main Content -->
 <main>
     <!-- Kurikulum -->
-    <section>
+    <section id="kurikulum-Kurikulum">
         <div class="container">
-            <div class="row">
+            <div class="row pt-5">
                 <div class="col">
-                    <h2 class="text-left fw-semibold pt-5">Kurikulum</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-3">
-                    <hr class="my-3">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-2 col-md-4">
-                    <div class="card text-bg-dark mx-2 my-2">
-                        <img src="https://picsum.photos/300/200" class="card-img" alt="https://picsum.photos/300/200">
-                        <div class="card-img-overlay">
-                            <!-- <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text">Last updated 3 mins ago</p> -->
+                    <h2 class="fs-2 text-center fw-semibold">Kurikulum Sekolah</h2>
+                    <div class="row">
+                        <div class="col-sm">
+                            <!-- Kosongkan -->
+                        </div>
+                        <div class="col-sm">
+                            <hr class="mt-1 mb-5">
+                        </div>
+                        <div class="col-sm">
+                            <!-- Kosongkan -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm">
+                            <p class="lh-lg fs-5 text-center">
+                                Kurikulum yang digunakan pada <b>Sekolah Dasar Negeri 1 Purwokerto Kulon</b> merupakan <b>Kurikulum 2013</b> dengan layanan SKS, sesuai dengan bakat, minat, dan kemampuan / kecepatan belajar peserta didik. Mengembangkan literasi, numerasi, meningkatkan kemampuan <b><i>Critical Thinking, Creativity, Collaboration, Communication</i></b> pada seluruh peserta didik.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -33,26 +34,144 @@
     <!-- Akhir dari Kurikulum -->
 
     <!-- Program -->
-    <section>
+    <section id="kurikulum-Program">
         <div class="container">
-            <div class="row">
+            <div class="row pt-5">
                 <div class="col">
-                    <h2 class="text-left fw-semibold pt-5">Program</h2>
+                    <h2 class="fs-2 text-left fw-semibold">Program</h2>
                 </div>
             </div>
             <div class="row">
                 <div class="col-3">
-                    <hr class="my-3">
+                    <hr class="mt-1 mb-5">
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-2 col-md-4">
-                    <div class="card text-bg-dark mx-2 my-2">
-                        <img src="https://picsum.photos/300/200" class="card-img" alt="https://picsum.photos/300/200">
-                        <div class="card-img-overlay">
-                            <!-- <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text">Last updated 3 mins ago</p> -->
+                <div class="col-sm">
+                    <p class="lh-lg fs-5 text" style="text-align: justify;">
+                        Seiringan dengan berjalannya Kurikulum tersebut, <b>Sekolah Dasar Negeri 1 Purwokerto Kulon</b> memiliki program-program yang dapat diikuti para peserta didik, yaitu sebagai berikut :
+                    </p>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Olimpiade MIPA</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Pramuka Siaga</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Lomba Kompetensi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Pramuka Penggalang Putra & Putri</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Kerohanian</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Kesenian</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Atletik</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  -->
+                <div class="col-sm-2 col-md-3 p-2">
+                    <div class="card" style="height: auto; max-width: 300px;">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center">
+                                <img src="assets/images/galeri/17.jpg" class="card-img" style="height: auto; width: full;" alt="Guru & Staff">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col p-2 d-flex justify-content-center" style="height: 50px;">
+                                <p class="text-center fw-semibold">Dokter Kecil / PMR</p>
+                            </div>
                         </div>
                     </div>
                 </div>
