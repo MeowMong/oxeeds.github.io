2466f2c95354b77bcfa445ad4ef1dc8795e70c44
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-08-11 21:41:53 +0700

    Update galeri.php

--- admin/galeri.php
+++ admin/galeri.php
@@ -1,23 +1,24 @@
-<?php include 'includes/admin_header.php'; ?>
-<?php include 'includes/admin_navigation.php' ?>
+<?php //include 'includes/admin_header.php'; ?>
+<?php //include 'includes/admin_navigation.php' ?>
 
 <?php
 
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-} else {
-    $page = '';
-}
+// if (isset($_GET['page'])) {
+    // $page = $_GET['page'];
+// } else {
+    // $page = '';
+// }
 
-switch ($page) {
-    case 'edit':
-        include 'includes/galeri/edit.php';
-        break;
-    default:
-        include 'includes/galeri/view.php';
-        break;
-}
+// switch ($page) {
+//     case 'edit':
+//         include 'includes/galeri/edit.php';
+//         break;
+//     default:
+//         include 'includes/galeri/view.php';
+//         break;
+// }
+
 
 ?>
 
-<?php include 'includes/admin_footer.php' ?>
+<?php //include 'includes/admin_footer.php' ?>
