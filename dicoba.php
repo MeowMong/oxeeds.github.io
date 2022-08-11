69a11347b48b0c9fe6221ccef121d64ad5a75a47
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-08-11 21:42:53 +0700

    Create dicoba.php

---
+++ dicoba.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>SD Negeri 1 Teluk</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="https://sdn1teluk.linknya.id/assets/img/kemendikbud.png" rel="icon">
+
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS Files -->
+    <link href="https://sdn1teluk.linknya.id/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS-->
+    <link href="https://sdn1teluk.linknya.id/assets/css/style2.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+        <div class="container d-flex justify-content-between align-items-center">
+
+            <div class="logo">
+                <h1 class="text-light">
+                    <a href="https://sdn1teluk.linknya.id">
+                        <img src="https://sdn1teluk.linknya.id/assets/img/logo2.png"></a>
+                </h1>
+            </div>
+
+            <!-- Navbar -->
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a href="https://sdn1teluk.linknya.id">Beranda</a></li>
+                    <li>
+                        <div class="dropdown">
+                            <a class="dropbtn active submenu">Profile</a>
+                            <div class="dropdown-content">
+                                <a href="https://sdn1teluk.linknya.id/profile/visidanmisi">Visi dan Misi</a>
+                                <a href="https://sdn1teluk.linknya.id/profile/gurudankaryawan">Guru dan Karyawan</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <a class="dropbtn ">Fasilitas</a>
+                            <div class="dropdown-content">
+                                <a href="https://sdn1teluk.linknya.id/fasilitas/perpustakaan">Perpustakaan</a>
+                                <a href="https://sdn1teluk.linknya.id/fasilitas/ruangguru">Ruang Guru</a>
+                                <a href="https://sdn1teluk.linknya.id/fasilitas/mushola">Mushola</a>
+                                <a href="https://sdn1teluk.linknya.id/fasilitas/kantin">Kantin</a>
+                                <a href="https://sdn1teluk.linknya.id/fasilitas/ruanglab">Ruang Lab</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a class="" href="https://sdn1teluk.linknya.id/berita">Berita</a></li>
+                    <li>
+                        <div class="dropdown">
+                            <a class="dropbtn ">Galeri</a>
+                            <div class="dropdown-content">
+                                <a href="https://sdn1teluk.linknya.id/galeri/galerifoto">Galeri Foto</a>
+                                <a href="https://sdn1teluk.linknya.id/galeri/galerivideo">Galeri Video</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a class="" href="https://sdn1teluk.linknya.id/kontak">Kontak</a></li>
+                    <li class="nav-item">
+                        <a href="https://sdn1teluk.linknya.id/login" class="nav-link">Login</a>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+            <!-- .navbar -->
+
+
+        </div>
+    </header>
+    <!-- End Header -->
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex justify-cntent-center align-items-center">
+        <div id="heroCarousel" class="container carousel carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+            <!-- Slide 1 -->
+            <div class="carousel-item active">
+                <div class="carousel-container">
+                    <div class="container">
+                        <div class="tengah title-hero-2">
+                            <span class="oren">VISI SD NEGERI 1 TELUK</span>
+                        </div>
+                        <div class="tengah title-hero">
+                            <h2 class="putih tittle-hero-2">"UNGGUL DAN RELIGI"</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- Indikator Visi -->
+        <section class="visi-misi section-bg">
+            <div class="container">
+                <h1 class="visi-misi-title oren mb-5">Indikator Visi</h1>
+                <ol class="visi-misi-list">1. Unggul dalam prestasi akademik dan non akademik<br />
+                    2. Unggul dalam budi pekerti<br />
+                    3. Unggul dalam Iman Taqwa (IMTAQ) dan IPTEK (Ilmu Pengetahuan dan Teknologi)<br />
+                    4. Unggul dalam nasionalisme.<br />
+                </ol>
+            </div>
+        </section>
+        <section class="visi-misi section-bg">
+            <div class="container">
+                <h1 class="visi-misi-title oren mb-5">Program Kerja SD N 1 Teluk</h1>
+                <ol class="visi-misi-list">1. Meningkatkan prestasi akademik dan non akademik agar mampu bersaing diera globalisasi di masa pandemi Covid-19.<br />
+                    2. Penerapan kurikulum 2013.<br />
+                    3. Menerapkan 18 pendidikan karakter.<br />
+                    4. Menerapkan rasa nasionalisme dan cinta tanah air.<br />
+                    5. Dibidang sarpras pembangunan talud ,tembok keliling, pengajuan ruang laboratorium TIK, dan perbaikan tempat parkir.<br />
+                    6. Pembinaan kegiatan ekstrakulikuler secara virtual dan luring.<br />
+                    7. Kegiatan pembelajaran secara tatap muka dan daring.<br />
+                    8. Persiapan sarana prasarana mendukung kegiatan tatap muka bagi siswa kelas 1-6.<br />
+                    9. Mengadakan kerja sama ( MOU ) dengan instansi lain yang mendukung pendidikan di SD.<br />
+                    10. Kegiatan pelatihan komputer bagi kelas 4-6.<br />
+                </ol>
+            </div>
+        </section>
+    </main>
+    <!-- End #main -->
+
+
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>SD Negeri 1 Teluk</span></strong>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <!-- Bootstrap JS -->
+    <script src="https://sdn1teluk.linknya.id/assets/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="https://sdn1teluk.linknya.id/assets/js/main.js"></script>
+</body>
+
+</html>
